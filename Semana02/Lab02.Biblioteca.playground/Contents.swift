b38a8f23e445d84ba5913e8d396b9fe0cd2d5baa
eder3105 <ederd.carrasco@tecsup.edu.pe>
2026-08-27 13:28:48 -0500

    agregar calculo

--- Semana02/Lab02.Biblioteca.playground/Contents.swift
+++ Semana02/Lab02.Biblioteca.playground/Contents.swift
@@ -41,3 +41,66 @@ let fechaDevolucionStr = readLine() ?? ""
 let fechaDevolucion = formatter.date(from: fechaDevolucionStr) ?? Date()
 
 //commit del ingreso de datos
+
+// MARK: - Calculo de fecha limite y dias de atraso
+let calendar = Calendar.current
+let fechaLimite = calendar.date(byAdding: .day, value: diasOtorgados, to: fechaPrestamo) ?? fechaPrestamo
+
+let diasAtrasoComponents = calendar.dateComponents([.day], from: fechaLimite, to: fechaDevolucion)
+var diasAtraso = diasAtrasoComponents.day ?? 0
+if diasAtraso < 0 {
+    diasAtraso = 0
+}
+
+// MARK: - Tarifa de multa segun tipo de usuario
+var tasaMulta = 0.0
+
+if tipoUsuario == "Alumno" {
+    tasaMulta = 1.5
+} else if tipoUsuario == "Docente" {
+    tasaMulta = 2.0
+} else if tipoUsuario == "Administrativo" {
+    tasaMulta = 3.0
+}
+
+// MARK: - Multa progresiva, dia por dia
+var multaTotal = 0.0
+
+if diasAtraso > 0 {
+    var dia = 1
+    while dia <= diasAtraso {
+        var tarifaDelDia = tasaMulta
+        if dia >= 4 && dia <= 6 {
+            tarifaDelDia = tasaMulta * 1.5
+        } else if dia >= 7 {
+            tarifaDelDia = tasaMulta * 2.0
+        }
+        multaTotal += tarifaDelDia
+        dia += 1
+    }
+}
+
+// MARK: - Situacion y estado
+var situacion = ""
+var usuarioHabilitado = true
+
+if diasAtraso == 0 {
+    situacion = "Devuelto a tiempo"
+} else if diasAtraso <= 3 {
+    situacion = "Atraso leve"
+} else if diasAtraso <= 6 {
+    situacion = "Atraso moderado"
+} else if diasAtraso <= 9 {
+    situacion = "Atraso grave"
+} else {
+    situacion = "Usuario suspendido"
+    usuarioHabilitado = false
+}
+
+var estado = "Devuelto"
+if diasAtraso > 0 {
+    estado = "Devuelto con atraso"
+}
+
+//commit del calculo
+
