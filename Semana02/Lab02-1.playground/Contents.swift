0348de565b80c43a5ed99555eb9d28a295f240a2
eder3105 <ederd.carrasco@tecsup.edu.pe>
2026-08-27 12:37:20 -0500

    ingreso de datos

---
+++ Semana02/Lab02-1.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+let  parcial = 0.3
+let trabajo = 0.3
+let final = 0.4
+
+print ("Nombre del Alumno")
+let alumno = readLine() ?? ""
+print("Nota del Examen Parcial : ")
+let exaparcial = Double(readLine() ?? "") ?? 0
+//commit del ingreso de datos
+print ("Nota de trabajo : ")
+let extrabajo = Double(readLine() ?? "") ?? 0
+print("Nota del Examen Final : ")
+let exfinal = Double(readLine() ?? "") ?? 0
+//commit del calculo
+//commit de mostrar los datos
