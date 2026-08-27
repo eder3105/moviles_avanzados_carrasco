105208a1164561d6df3145e92a060460e143b821
Sebastian Salas <sebastian.salas@tecsup.edu.pe>
2026-08-27 12:05:56 -0500

    Agregar Lab02

---
+++ Semana02/Lab02.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+print("Numero 1")
+
+let a = Double(readLine() ?? "") ?? 0
+print("Numero 2")
+let b = Double(readLine() ?? "") ?? 0
+
+let suma = a + b
+print ("La suma es \(suma)")
