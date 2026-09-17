feed4c1e62dd6e83057b708271137531474aaf6a
eder3105 <ederd.carrasco@tecsup.edu.pe>
2026-09-17 10:54:29 -0500

    feat: implementa estructura Factura y logica de descuentos de Tecsup

--- Semana04/Actividad01/main.swift
+++ Semana04/Actividad01/main.swift
@@ -28,3 +28,36 @@ let catalogoCursos: [CursoCatalogo] = [
     CursoCatalogo(id: 3, nombre: "Diseño UX/UI", precioUnitario: 500.00),
     CursoCatalogo(id: 4, nombre: "Desarrollo Web Fullstack", precioUnitario: 550.00)
 ]
+
+struct Factura {
+    let estudiante: Estudiante
+    let cursos: [CursoComprado]
+    
+    var totalCantidadCursos: Int {
+        return cursos.reduce(0) { $0 + $1.cantidad }
+    }
+    
+    var subtotal: Double {
+        return cursos.reduce(0.0) { $0 + $1.total }
+    }
+    
+    var igv: Double {
+        return subtotal * 0.18
+    }
+    
+    var totalConIGV: Double {
+        return subtotal + igv
+    }
+    
+    var descuentoCantidad: Double {
+        return totalCantidadCursos >= 3 ? (totalConIGV * 0.10) : 0.0
+    }
+    
+    var descuentoTecsup: Double {
+        return (estudiante.esAlumnoTecsup && totalCantidadCursos >= 3) ? 400.0 : 0.0
+    }
+    
+    var totalFinal: Double {
+        return totalConIGV - descuentoCantidad - descuentoTecsup
+    }
+}
