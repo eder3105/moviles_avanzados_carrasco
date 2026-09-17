1972174d0822e069d6cf2c50487148f623c05c93
eder3105 <ederd.carrasco@tecsup.edu.pe>
2026-09-17 11:08:09 -0500

    feat: agrega subclases ClienteNatural y ClienteJuridico heredando de Cliente

--- Semana04/Actividad02/main.swift
+++ Semana04/Actividad02/main.swift
@@ -23,3 +23,42 @@ class Cliente {
         print(String(format: "💰 Monto mínimo de apertura: S/ %.2f", montoMinimoApertura))
     }
 }
+
+class ClienteNatural: Cliente {
+    var nombreCompleto: String
+    var dni: String
+    
+    init(codigo: String, direccion: String, fechaDeRegistro: String, numeroCuenta: String, montoMinimoApertura: Double, nombreCompleto: String, dni: String) {
+        self.nombreCompleto = nombreCompleto
+        self.dni = dni
+        super.init(codigo: codigo, direccion: direccion, fechaDeRegistro: fechaDeRegistro, numeroCuenta: numeroCuenta, montoMinimoApertura: montoMinimoApertura)
+    }
+    
+    override func mostrarDatos() {
+        print("\n👤 Cliente Natural:")
+        print("Nombre: \(nombreCompleto)")
+        print("DNI: \(dni)")
+        super.mostrarDatos()
+    }
+}
+
+class ClienteJuridico: Cliente {
+    var razonSocial: String
+    var ruc: String
+    var representanteLegal: String
+    
+    init(codigo: String, direccion: String, fechaDeRegistro: String, numeroCuenta: String, montoMinimoApertura: Double, razonSocial: String, ruc: String, representanteLegal: String) {
+        self.razonSocial = razonSocial
+        self.ruc = ruc
+        self.representanteLegal = representanteLegal
+        super.init(codigo: codigo, direccion: direccion, fechaDeRegistro: fechaDeRegistro, numeroCuenta: numeroCuenta, montoMinimoApertura: montoMinimoApertura)
+    }
+    
+    override func mostrarDatos() {
+        print("\n🏢 Cliente Jurídico:")
+        print("Razón Social: \(razonSocial)")
+        print("RUC: \(ruc)")
+        print("Representante Legal: \(representanteLegal)")
+        super.mostrarDatos()
+    }
+}
